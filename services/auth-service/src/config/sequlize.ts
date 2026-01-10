2f61b616fcc954f3e723c5bba1cb8eee2d255942
Midhun-u <midhunvkm689@gmail.com>
2026-01-10 11:18:53 +0530

    Added media service for adding images

--- services/auth-service/src/config/sequlize.ts
+++ services/auth-service/src/config/sequlize.ts
@@ -4,10 +4,10 @@ import { envVariables } from "../utils/envVariables.js";
 // Sequelize configuration
 export const sequelize = new Sequelize({
     dialect: "postgres",
-    database: envVariables.DB_DATABASE_NAME as string,
-    username: envVariables.DB_USERNAME as string,
-    password: envVariables.DB_PASSWORD as string,
-    host: envVariables.DB_HOST as string,
+    database: envVariables.DB_DATABASE_NAME,
+    username: envVariables.DB_USERNAME,
+    password: envVariables.DB_PASSWORD,
+    host: envVariables.DB_HOST,
     port: Number(envVariables.DB_PORT),
     logging: false
 })
--- services/auth-service/src/utils/envVariables.ts
+++ services/auth-service/src/utils/envVariables.ts
@@ -3,12 +3,18 @@ import 'dotenv/config'
 // ENV variables
 export const envVariables = {
     PORT: process.env.PORT,
-    DB_DATABASE_NAME: process.env.DB_DATABASE_NAME,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    ADMIN_KEY: process.env.ADMIN_KEY,
-    CLIENT_URL: process.env.CLIENT_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    DB_DATABASE_NAME: process.env.DB_DATABASE_NAME as string as string,
+    DB_USERNAME: process.env.DB_USERNAME as string,
+    DB_PASSWORD: process.env.DB_PASSWORD as string,
+    DB_HOST: process.env.DB_HOST as string,
+    DB_PORT: process.env.DB_PORT as string,
+    ADMIN_KEY: process.env.ADMIN_KEY as string,
+    CLIENT_URL: process.env.CLIENT_URL as string,
+    JWT_SECRET: process.env.JWT_SECRET as string,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY as string,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN as string,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID as string,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET as string,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID as string,
 }
