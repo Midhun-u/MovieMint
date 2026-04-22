f2903fc27c25ce253e7d2103d447a5ed8100930c
Midhun U <midhunvkm689@gmail.com>
2026-04-22 10:14:23 +0530

    Updated the project

--- services/media-service/src/controllers/user/getUserImage.controller.ts
+++ services/media-service/src/controllers/user/getUserImage.controller.ts
@@ -13,7 +13,6 @@ export const getUserImageController = handleError(async (request: Request, respo
     }
    
     const userImage = await UserImageModel.getImageByUserId(userId, ["id", "image_url", "user_id"])
-    console.log(userImage)
   
     return sendResponse(response, true, 200, null, {userImage: userImage || null})
 
